623f190e7aae4dadd457a10933d93bffe3c7fec8
edbaafi <ed@modk.it>
2018-10-05 17:31:38 -0400

    Update mphalport.h

--- ports/javascript/mphalport.h
+++ ports/javascript/mphalport.h
@@ -28,7 +28,8 @@
 #include "lib/utils/interrupt_char.h"
 
 #define mp_hal_stdin_rx_chr() (0)
-//void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len);
+#define __mp_hal_stdout_tx_strn_override__
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len);
 
 void mp_hal_delay_ms(mp_uint_t ms);
 void mp_hal_delay_us(mp_uint_t us);
